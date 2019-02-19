220fdf8c7d9676f1544859444cdca12d696c620e
mgthihazaw <mgthihazaw777@gmail.com>
2019-02-19 09:55:13 +0630

    CRUD for Question along with Question Resource

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,6 +9,23 @@ use App\User;
 
 class Question extends Model
 {
+
+    protected $fillable=[
+         'title',
+         'slug',
+         'body',
+         'category_id',
+         'user_id',
+    ];
+    public function getRouteKeyName()
+    {
+       return 'slug';
+    }
+
+
+
+
+
     public function user()
     {
     	return $this->belongsTo(User::class);
@@ -19,5 +36,8 @@ class Question extends Model
     }
     public function category(){
     	return $this->belongsTo(Category::class);
+    }
+    public function getPathAttribute(){
+        return asset("api/question/$this->slug");
     }
 }
